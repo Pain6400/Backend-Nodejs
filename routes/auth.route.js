2e15dd9c6a0ad9bce26f36b22016322a0c04220f
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2022-08-20 22:07:43 -0600

    email

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { login, register } from '../controllers/auth.controller.js';
+import { body } from 'express-validator';
 const router = express.Router();
 
 router.get('/login', login);
 
-router.post('/register', register)
+router.post('/register', [
+    body('email', "Formato incorrecto").isEmail().normalizeEmail()
+], register)
 
 
 export default router;
