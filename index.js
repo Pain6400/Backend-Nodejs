4f8cbed36fd8072615373fb3b94b2bab540348df
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2022-11-08 15:45:02 -0600

    fix

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("https://localhost:" + PORT))
+app.listen(PORT, () => console.log("http://localhost:" + PORT))
